6c81b89de159e1d959c0a15d19e1a840792443cd
Joel Van Auken <JVanAuken@misoenergy.org>
2024-12-10 09:15:32 -0500

    day 10 scaffold

---
+++ src/day10.py
@@ -0,0 +1,15 @@
+from Helpers import *
+
+
+def part1():
+    pass
+
+def part2():
+    pass
+
+example = True
+input = read_input(10, example, True)
+
+execute(part1, "Day 10 - Part 1" + (" - Example" if example else ""))
+# Output:  - 
+# execute(part2, "Day 10 - Part 2" + (" - Example" if example else ""))
