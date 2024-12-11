71b003f6af100d5e172daa3de9e3f159638bacfc
Joel Van Auken <JVanAuken@misoenergy.org>
2024-12-11 14:19:54 -0500

    Day 8 pt 2

--- src/day8.py
+++ src/day8.py
@@ -1,26 +1,23 @@
 from Helpers import *
 
-def find_antinodes_by_freq(freq, locs):
+def find_antinodes_by_freq(freq, locs, forever=False):
     anodes = set()
-    # print(freq)
-    # print(locs)
     for i, a in enumerate(locs):
-        # print(f"a: {a}")
         for j, b in enumerate(locs, i + 1):
             if a == b:
                 continue
-            # print(f"b: {b}")
             vector = vect(a, b)
-            # print(vector)
             v1 = b[0] + vector[0], b[1] + vector[1]
-            v2 = a[0] - vector[0], a[1] - vector[1]
-            if v1[0] >= 0 and v1[0] < len(input) and v1[1] >= 0 and v1[1] < len(input[0]) and (v1 != (0,0)):
-                # print(f'adding {v1}')
+            while v1[0] >= 0 and v1[0] < len(input) and v1[1] >= 0 and v1[1] < len(input[0]):
                 anodes.add(v1)
-            if v2[0] >= 0 and v2[0] < len(input) and v2[1] >= 0 and v2[1] < len(input[0]) and (v2 != (0,0)):
-                # print(f'adding {v2}')
+                v1 = v1[0] + vector[0], v1[1] + vector[1]
+
+            v2 = a[0] - vector[0], a[1] - vector[1]
+            while v2[0] >= 0 and v2[0] < len(input) and v2[1] >= 0 and v2[1] < len(input[0]):
                 anodes.add(v2)
-            # print(anodes)
+                v2 = v2[0] - vector[0], v2[1] - vector[1]
+            anodes.add(a)
+            anodes.add(b)
         # for i, r in enumerate(input):
         #     for j, c in enumerate(r):
         #         print(input[i][j] if (i, j) not in anodes else '#', end='')
@@ -44,7 +41,7 @@ def find_antennas():
                     antennas[c].extend([(i, j)])
     return antennas
 
-def find_antinodes(antennas):
+def find_antinodes(antennas, forever=False):
     antinodes = set()
     for a in antennas:
         anodes = find_antinodes_by_freq(a, antennas[a])
@@ -63,15 +60,21 @@ def part1():
     return len(antinodes)
 
 def part2():
-    pass
+    antennas = find_antennas()
+    antinodes = find_antinodes(antennas, True)
+    # for i, r in enumerate(input):
+    #     for j, c in enumerate(r):
+    #         print(input[i][j] if (i, j) not in antinodes else '#', end='')
+    #     print()
+    return len(antinodes)
 
 example = False
 input = read_input(8, example, False)
 
-execute(part1, "Day 8 - Part 1" + (" - Example" if example else ""))
+# execute(part1, "Day 8 - Part 1" + (" - Example" if example else ""))
 # Result: 390
 # Operation took 3.412700 ms.
-# execute(part2, "Day 8 - Part 2" + (" - Example" if example else ""))
+execute(part2, "Day 8 - Part 2" + (" - Example" if example else ""))
 # Output: 0 - 0.00 ms
 
 
