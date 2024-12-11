f0f43d02b96b92884bcbaabc9cd321b8072ba24f
Joel Van Auken <JVanAuken@misoenergy.org>
2024-12-11 09:25:09 -0500

    Day 11 pt 1

--- src/day11.py
+++ src/day11.py
@@ -1,13 +1,31 @@
 from Helpers import *
 
+def stone_blink(stones):
+    new_state = []
+    for stone in stones:
+        if stone == 0:
+            new_state.append(1)
+        elif len(str(stone)) % 2 == 0:
+            s = str(stone)
+            new_state.append(int(s[:len(s)//2]))
+            new_state.append(int(s[len(s)//2:]))
+        else:
+            new_state.append(stone * 2024)
+    return new_state
+
 def part1():
-    pass
+    stones = [int(x) for x in input.split(' ')]
+    
+    for i in range(25):
+        stones = stone_blink(stones)
+        # print(stones)
+    return len(stones)
+
 def part2():
     pass
 
-# example = True
 example = False
-input = read_input(11, example, False)
+input = read_input(11, example, True)
 
 execute(part1, "Day 11 - Part 1" + (" - Example" if example else ""))
 # Output: 0 - 0.00 ms
