154f14328bdce7b201dfc5a181a296ef61c30e0f
Joel Van Auken <JVanAuken@misoenergy.org>
2024-12-11 11:39:39 -0500

    Day 11 pt 2. thanks Evan.

--- src/day11.py
+++ src/day11.py
@@ -1,4 +1,14 @@
 from Helpers import *
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+def blink(stone):
+    if stone == 0:
+        return [1]
+    elif len(str(stone)) % 2 == 0:
+        s = str(stone)
+        return [int(s[:len(s)//2]), int(s[len(s)//2:])]
+    else:
+        return [stone * 2024]
 
 def stone_blink(stones):
     new_state = []
@@ -13,6 +23,22 @@ def stone_blink(stones):
             new_state.append(stone * 2024)
     return new_state
 
+
+def count_levels(stone, level, target):
+    count = 0
+    if level == target:
+        level_memos[level][stone] = 1
+        return 1
+    if stone in level_memos[level]:
+        return level_memos[level][stone]
+    nxt_level = blink(stone)
+    for nxt_stone in nxt_level:
+        ct = count_levels(nxt_stone, level + 1, target)
+        count += ct
+    level_memos[level][stone] = count
+    return count
+            
+
 def part1():
     stones = [int(x) for x in input.split(' ')]
     
@@ -21,13 +47,23 @@ def part1():
         # print(stones)
     return len(stones)
 
+depth = 75
+level_memos = [{} for _ in range(depth + 1)]
+
 def part2():
-    pass
+    stones = [int(x) for x in input.split(' ')]
+    count = 0
+    for stone in stones:
+        count += count_levels(stone, 0, depth)
+
+    return count
 
 example = False
 input = read_input(11, example, True)
 
-execute(part1, "Day 11 - Part 1" + (" - Example" if example else ""))
-# Output: 0 - 0.00 ms
+# execute(part1, "Day 11 - Part 1" + (" - Example" if example else ""))
+# Output: 198089 - 513.981800 ms
 execute(part2, "Day 11 - Part 2" + (" - Example" if example else ""))
-# Output: 0 - 0.00 ms
+# Result: 236302670835517
+# Operation took 382.750400 ms.
+
